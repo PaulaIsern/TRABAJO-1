3050d1ea8b4a0149379903be97a709f37b0b3f05
PaulaIsern <paulaisernestarellas@gmail.com>
2024-10-16 00:25:00 +0200

    MENU CON HOVER DE TEXTO

---
+++ script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     // Al hacer hover en el texto, cambiar la imagen de fondo
+//     $('.hover-effect').hover(
+//         function() {
+//             $('body').removeClass('bg-image1').addClass('bg-image2'); // Cambia a la nueva imagen
+//         },
+//         function() {
+//             $('body').removeClass('bg-image2').addClass('bg-image1'); // Vuelve a la imagen original
+//         }
+//     );
+// });
