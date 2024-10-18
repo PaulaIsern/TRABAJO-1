99e87505756465bd904efe1c30171476785663b7
PaulaIsern <paulaisernestarellas@gmail.com>
2024-10-18 15:06:46 +0200

    MENU HOVER TERMINADO

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const hoverLinks = document.querySelectorAll('.hover-link');
 // Cambiar el color del fondo 
 hoverLinks.forEach(link => {
     link.addEventListener('mouseover', () => {
-        const newBgColor = link.getAttribute('data-bg');
-        document.body.style.backgroundColor = newBgColor;
+        const newImg= link.getAttribute('data-bg');
+        document.body.style.backgroundImage = 'url(' + newImg + ')';
     });
 
     link.addEventListener('mouseout', () => {
