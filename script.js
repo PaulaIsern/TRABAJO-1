ee215d47223a61c74e07d641d5efac927077ce5f
PaulaIsern <paulaisernestarellas@gmail.com>
2024-10-19 15:06:45 +0200

    imagenes

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ hoverLinks.forEach(link => {
         document.body.style.backgroundColor = '#1a1a1a';
     });
 });
+
+
+//IMAGENES TRANSICION//
+
+
+// Mostrar las imágenes al hacer scroll
+function mostrarImagenesAlHacerScroll() {
+    const seccionInformacion = document.getElementById('informacion');
+    const posicionSeccion = seccionInformacion.getBoundingClientRect().top;
+    const alturaPantalla = window.innerHeight;
+
+    // Verifica si la sección es visible en la pantalla
+    if (posicionSeccion < alturaPantalla) {
+        const imagenes = seccionInformacion.querySelectorAll('img');
+        imagenes.forEach(imagen => {
+            imagen.classList.add('img-visible'); // Añadir clase para mostrar la imagen
+        });
+
+        // Dejar de escuchar el scroll una vez que las imágenes se muestran
+        window.removeEventListener('scroll', mostrarImagenesAlHacerScroll);
+    }
+}
+
+// Escuchar el evento de scroll
+window.addEventListener('scroll', mostrarImagenesAlHacerScroll);
+
