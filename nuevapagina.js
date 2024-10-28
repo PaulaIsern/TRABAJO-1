ce568340a2edb22cc9dba719e03d6ad1ce3fb972
MilenaManosalvas <milenamanosalvasv@gmail.com>
2024-10-28 22:59:42 +0100

    Boton flecha detras,

--- nuevapagina.js
+++ nuevapagina.js
@@ -10,5 +10,42 @@
 //     }
 
 //     setInterval(showNext, 2000); // Cambia cada 3 segundos
-// });
+// });‚¿¿`
+
+
+
+
+// Selecciona el botón de la hamburguesa y el contenido a ocultar
+const navbarToggle = document.querySelector('.navbar-toggler');
+const contentToHide = document.getElementById('contentToHide');
+
+// Al hacer clic en el botón de la hamburguesa
+navbarToggle.addEventListener('click', () => {
+    // Verifica si el menú está abierto
+    if (navbarToggle.getAttribute('aria-expanded') === 'true') {
+        // Si está abierto, oculta el contenido
+        contentToHide.style.display = 'none';
+    } else {
+        // Si está cerrado, muestra el contenido
+        contentToHide.style.display = 'block';
+    }
+});
+
+// Opcional: Asegurarse de que el contenido esté visible al cerrar el menú
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!navbarToggle.contains(target) && !document.getElementById('navbarToggleExternalContent').contains(target)) {
+        // Cuando se hace clic fuera del menú, muestra el contenido
+        contentToHide.style.display = 'block';
+    }
+});
+
+
+function toggleSearchBar() {
+    const searchInput = document.getElementById("searchInput");
+    searchInput.classList.toggle("d-none");
+    searchInput.focus();
+}
+
+
 
