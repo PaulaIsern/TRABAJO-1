a5be673ad1456d070f3773e6053beded3ab17f4a
MilenaManosalvas <milenamanosalvasv@gmail.com>
2024-11-06 01:09:07 +0100

    Responsive, galeria nueva con efecto y javascript

--- nuevapagina.js
+++ nuevapagina.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const carousel = document.getElementById('carouselExample');
-//     const carouselItems = carousel.querySelectorAll('.carousel-item');
-//     let currentIndex = 0;
-
-//     function showNext() {
-//         carouselItems[currentIndex].classList.remove('active');
-//         currentIndex = (currentIndex + 1) % carouselItems.length;
-//         carouselItems[currentIndex].classList.add('active');
-//     }
-
-//     setInterval(showNext, 2000); // Cambia cada 3 segundos
-// });‚¿¿`
-
-
 
 
 // Selecciona el botón de la hamburguesa y el contenido a ocultar
@@ -46,6 +31,8 @@ function toggleSearchBar() {
     searchInput.classList.toggle("d-none");
     searchInput.focus();
 }
+
+
 
 
 
